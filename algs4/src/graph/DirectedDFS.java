782cb203e8bed0829add36fc288827d562396367
shuxiaoyuan <1362521868@qq.com>
2018-09-01 17:04:40 +0800

    有向图的算法

---
+++ algs4/src/graph/DirectedDFS.java
@@ -0,0 +1,39 @@
+package graph;
+
+/**
+ * 
+ * 有向图的可达性
+ *
+ */
+public class DirectedDFS {
+    private boolean[] marked;
+    
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+    
+    // 一个集合 sources 中是否存在点到目标是可达的
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for(int s : sources) {
+            if(!marked[s]) {
+                dfs(G, s);
+            }
+        }
+    }
+    
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        for(int w : G.adj(v)) {
+            if(!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+    
+    public boolean marked(int v) {
+        return marked[v];
+    }
+    
+}
---
+++ algs4/src/graph/Topological.java
@@ -0,0 +1,22 @@
+package graph;
+
+public class Topological {
+    private Iterable<Integer> order;    // 顶点的拓扑排序
+    
+    public Topological(Digraph G) {
+        DirectedCycle cyclefinder = new DirectedCycle(G);
+        if(!cyclefinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
+    }
+    
+    public Iterable<Integer> order() {
+        return order;
+    }
+    
+    public boolean isDAG() {
+        return order != null;
+    }
+    
+}

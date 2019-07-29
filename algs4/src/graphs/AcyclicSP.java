afb6e038f269818072e2bf37522c866056f3ad9e
lishuyuan <lishuyuan@doodlemobile.com>
2019-07-29 15:38:55 +0800

    拓扑排序最短路径

---
+++ algs4/src/graphs/AcyclicSP.java
@@ -0,0 +1,44 @@
+package graphs;
+
+
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.Topological;
+
+/**
+ * Author: lsy
+ * Date&Time: 2019/7/29 14:40
+ * Description: P427 算法 4.10 无环加权有向图的最短路径算法
+ */
+public class AcyclicSP {
+
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+
+    public AcyclicSP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+
+        for(int v = 0; v < G.V(); ++v) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+
+        Topological top = new Topological(G);
+
+        for(int v : top.order()) {
+            relax(G, v);
+        }
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+            }
+        }
+    }
+
+}
